b80c56331dcbad04fe858ae9b73f715c7049b5bd
Fernando Alves <fx2.sistema@gmail.com>
2021-07-30 20:59:51 -0300

    perfil migration

---
+++ database/migrations/2021_07_30_205559_create_perfils_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerfilsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfils', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 190);
+            $table->unsignedInteger('setor_id');
+            $table->boolean('status');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('setor_id')->references('id')->on('setors')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfils');
+    }
+}
