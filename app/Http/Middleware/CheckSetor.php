6adb2213f75a44bf539c76bc442ca5a0a8833d9c
Fernando Alves <fx2.sistema@gmail.com>
2021-08-01 16:24:44 -0300

    perfil ta top2

---
+++ app/Http/Middleware/CheckSetor.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Permisso;
+use App\Models\PermissoesUsuario;
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckSetor
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $id)
+    {
+        $permissao = $this->checarPermissao($id);
+
+        if (!$permissao)
+            return redirect('home');
+
+        return $next($request);
+    }
+
+    /*
+     * Se dada permissao existe dentro do rol de permissao, retorna-se true
+     * */
+    public function checarPermissao(int $idPermissao){
+        $user = auth('api')->user();
+
+        return PermissoesUsuario::where('setor_id', $user->setor_id)
+            ->where('perfil_id', $user->perfil_id)
+            ->where('idpermissao', $idPermissao)
+            ->exists();
+    }
+}
