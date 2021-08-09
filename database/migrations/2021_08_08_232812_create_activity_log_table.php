f2a192fdc42f009b7f74ea9fd717c6d8ff1ab84c
Fernando Alves <fx2.sistema@gmail.com>
2021-08-09 08:43:52 -0300

    saida nos ultimos vai

--- database/migrations/2021_08_08_232812_create_activity_log_table.php
+++ database/migrations/2021_08_08_232812_create_activity_log_table.php
@@ -14,7 +14,7 @@ class CreateActivityLogTable extends Migration
             $table->text('description');
             $table->nullableMorphs('subject', 'subject');
             $table->nullableMorphs('causer', 'causer');
-            $table->json('properties')->nullable();
+            $table->longText('properties')->nullable();
             $table->unsignedInteger('setor_id')->nullable();
             $table->string('setor')->nullable();
             $table->string('nome')->nullable();
