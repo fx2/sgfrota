3df5bd7b2b7959c1c2d4cc2cc5e75e2faf381162
Fernando Alves <fx2.sistema@gmail.com>
2021-08-09 17:33:42 -0300

    moto b

--- app/Models/Motoristum.php
+++ app/Models/Motoristum.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Motoristum extends BaseModel
+class Motoristum extends Model
 {
     use SoftDeletes;
 
