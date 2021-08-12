f11d6c98d958a77d2890117681718c6dd77b452b
Fernando Alves <fx2.sistema@gmail.com>
2021-08-12 08:58:02 -0300

    bg

--- public/js/ajax_motorista.js
+++ public/js/ajax_motorista.js
@@ -18,17 +18,57 @@ async function loadMotorista(motorista_id = null){
 
     const resp = await axios.get(`${BASE_URL}/motorista?with=fornecedor,tipoCnh&where=id,=,${motorista_id}&first=true`);
 
+    validade = cnhAvisarAntesXdias(resp.data);
+
     motoristaAppend.after(
         `
             <ul class="ml-3 list-unstyled" id="motorista-remove-append">
                 <li><strong>Proprietário</strong>: ${resp.data.nome} </li>
                 <li><strong>Tipo CNH</strong>: ${resp.data.tipo_cnh.nome}</li>
                 <li><strong>CNH</strong>: ${resp.data.cnh}</li>
-                <li><strong>Validade da CNH</strong>: ${moment(resp.data.cnh_validade).format('DD/MM/YYYY')}</li>
+                <li ${validade ? 'style="color: red;"' : ''}><strong>Validade da CNH</strong>: ${moment(resp.data.cnh_validade).format('DD/MM/YYYY')}</li>
                 <li><strong>RG</strong>: ${resp.data.rg}</li>
                 <li><strong>CPF</strong>: ${resp.data.cpf}</li>
                 <li><strong>Setor</strong>: ${resp.data.setor.nome}</li>
             </ul>
         `
     );
+}
+
+
+function cnhAvisarAntesXdias(dateInfo){
+    let dateSub = moment(dateInfo.cnh_validade).subtract(dateInfo.avisar_antes_qtddias, "days").format("YYYY-MM-DD")
+    let compareDate = moment(moment().format("YYYY-MM-DD"), "YYYY-MM-DD");
+    let startDate   = moment(dateSub, "YYYY-MM-DD");
+    let endDate     = moment(dateInfo.cnh_validade, "YYYY-MM-DD");
+
+    // console.log('cnh validade', moment(dateInfo.cnh_validade).format("YYYY-MM-DD"));
+    // console.log('qtd dias', dateInfo.avisar_antes_qtddias);
+    // console.log('avisar antes', moment(dateInfo.cnh_validade).subtract(dateInfo.avisar_antes_qtddias, "days").format("YYYY-MM-DD"));
+    // console.log('hoje', moment().format("YYYY-MM-DD"))
+    // console.log('compare', compareDate.isBetween(startDate, endDate));
+    // console.log('igualcomeco', moment(compareDate).isSame(startDate));
+    // console.log('igualfinal', moment(compareDate).isSame(endDate));
+    //
+    // console.log('---------------------------------')
+
+    compare = compareDate.isBetween(startDate, endDate);
+    igualcomeco = moment(compareDate).isSame(startDate);
+    igualfinal = moment(compareDate).isSame(endDate);
+    compareAfter = moment(compareDate).isAfter(endDate);
+
+    if (compare)
+        return true;
+
+    if (igualcomeco)
+        return true;
+
+    if (igualfinal)
+        return true;
+
+    if (compareAfter)
+        return true;
+
+    return false;
+
 }
