54a8617f5a2427612d9e5845706d651f7b101009
Fernando <fx2.sistemas@gmail.com>
2021-09-28 07:52:05 -0300

    dunga burru

--- resources/views/parts/imagem-pdf.blade.php
+++ resources/views/parts/imagem-pdf.blade.php
@@ -1,7 +1,7 @@
 <div class="col-10">
     <label for="documento" class="control-label">{{ '' }}</label>
     @if (verifyImagePDF($value, '.pdf'))
-        <a href="{{ removePublicPath(asset($value)) }}" target="_blank">Veisualizar documento PDF</a>
+        <a href="{{ removePublicPath(asset($value)) }}" target="_blank">Visualizar documento PDF</a>
     @else
         <a href="{{ removePublicPath(asset($value)) }}" target="_blank">
             <img class="img-fluid" id="{{ $id ?? ''}}" src="{{ isset($value) ? removePublicPath(asset($value)) : '' }}" alt="{{ isset($value) ? $value : '' }}" >
