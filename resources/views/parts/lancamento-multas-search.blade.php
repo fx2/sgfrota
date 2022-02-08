7b2cd5ea24f7008e6cca7a6041d00e83280c16b3
nandao <fx2.sistemas@gmail.com>
2022-02-08 16:25:12 -0300

    codigo modal

--- resources/views/parts/lancamento-multas-search.blade.php
+++ resources/views/parts/lancamento-multas-search.blade.php
@@ -1,16 +1,21 @@
 <div class="col-12 col-sm-6	col-md-6 col-lg-6 col-xl-6 mb-1">
+    <label for="numero_ait">Número AIT</label>
     <div class="form-group">
-        <label for="exampleFormControlInput1">Número AIT</label>
-        <select name="numero_ait" class="form-control" id="numero_ait">
+        <select name="numero_ait" class="form-control inside_modal" id="numero_ait">
             <option value="">Selecione ...</option>
-
         </select>
     </div>
 </div>
 
 @push('js')
 <script>
-    setTimeout(loadAit, 3000);
+    $(document).ready(function() {
+      $(".inside_modal").select2({
+        dropdownParent: $("#exampleModal")
+      });
+    });
+
+    loadAit();
 
     async function loadAit() {
         const resp = await axios.get(`${BASE_URL}/lancamento-multas?select=numero_ait&get=true`);
